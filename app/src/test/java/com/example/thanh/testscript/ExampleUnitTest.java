7896b865bd52419b6db1b7245c36ffb1e5bed23d
cpu60058 <thanhvantran0@gmail.com>
2018-11-08 10:54:50 +0700

    Chuyen qua testng, them dataProvider

--- app/src/test/java/com/example/thanh/testscript/ExampleUnitTest.java
+++ app/src/test/java/com/example/thanh/testscript/ExampleUnitTest.java
@@ -2,10 +2,13 @@ package com.example.thanh.testscript;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -26,19 +29,10 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     MobileDriver driver;
 
-    @Test
-    public void testRun() {
+    @Test(dataProvider = "DataTest")
+    public void testRun(String sTenChuyenDi, String sTenDiemDi, String sTenDiemDen, String sSoTien) {
         System.out.println("testrun");
 
-//        Tim nut dang nhap face
-//        MobileElement btnLogin = (MobileElement) driver.findElement(By.xpath("//android.widget.TextView[@text='Đăng nhập Facebook']"));
-//        if(btnLogin.isDisplayed()) {
-//            System.out.println("Thay thang face");
-//            btnLogin.click();
-//            driver.findElement(By.id("m_login_email")).sendKeys("thanhvantran052@gmail.com");
-//            driver.findElementById("m_login_password").sendKeys("01692982109as");
-//            driver.findElementById("u_0_5").click();
-//        }
         AndroidTouchAction touchAction = new AndroidTouchAction(driver);
 //        Bấm vào nút thêm mới
         MobileElement btnAdd = (MobileElement) driver.findElement(By.xpath("//android.widget.TextView[@text='+']"));
@@ -47,12 +41,12 @@ public class ExampleUnitTest {
             MobileElement txtTenChuyenDi = (MobileElement) driver.findElementByXPath("//android.widget.EditText[@instance='0']");
             touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(txtTenChuyenDi))).perform();
             txtTenChuyenDi.clear();
-            txtTenChuyenDi.setValue("Ha Noi");
+            txtTenChuyenDi.setValue(sTenChuyenDi);
 
             MobileElement txtDiemDi = (MobileElement) driver.findElementByXPath("//android.widget.EditText[@instance='1']");
             touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(txtDiemDi))).perform();
             txtDiemDi.clear();
-            txtDiemDi.setValue("Ha Noi");
+            txtDiemDi.setValue(sTenDiemDi);
 
             MobileElement diaDiemDiDauTien = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@instance='3']");
             assertEquals(diaDiemDiDauTien.isDisplayed(),true);
@@ -61,7 +55,7 @@ public class ExampleUnitTest {
             MobileElement txtDiemDen = (MobileElement) driver.findElementByXPath("//android.widget.EditText[@instance='2']");
             touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(txtDiemDen))).perform();
             txtDiemDen.clear();
-            txtDiemDen.setValue("Da Nang");
+            txtDiemDen.setValue(sTenDiemDen);
 
             MobileElement diaDiemDenDauTien = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@instance='4']");
             assertEquals(diaDiemDenDauTien.isDisplayed(),true);
@@ -72,7 +66,7 @@ public class ExampleUnitTest {
             el7.click();
 
             MobileElement txtSoTien = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[2]");
-            txtSoTien.sendKeys("12050");
+            txtSoTien.sendKeys(sSoTien);
 
             MobileElement dauTich = (MobileElement) driver.findElementByXPath("//android.widget.ImageView[@instance='1']");
             touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(dauTich))).perform();
@@ -91,6 +85,12 @@ public class ExampleUnitTest {
             dauTich = (MobileElement) driver.findElementByXPath("//android.widget.ImageView[@instance='1']");
             touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(dauTich))).perform();
 
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
 //            Phan so sanh xem chuyen di co duoc tao hay khong
             MobileElement txtTen = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@instance='2']");
             assertEquals(txtTen.getText(),"Hotake Kakashi");
@@ -100,9 +100,6 @@ public class ExampleUnitTest {
 
         }
 
-//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-//        WebElement message = driver.findElement(By.id("android:id/message"));
-//        Assert.assertEquals(message.getText(),sExpectedResult);
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -110,25 +107,21 @@ public class ExampleUnitTest {
         }
     }
 
-//    @DataProvider(name = "newData")
-//    public static Object[][] datas() {
-//        return new Object[][] {
-//                {"aaa","123","Dang nhap thanh cong"},
-//                {"20","142","Dang nhap that bai"},
-//                {"aaa","125","Dang nhap thanh cong"},
-//                {"awfaa","ss","Dang nhap that bai"}
-//        };
-//    }
+    @DataProvider(name = "DataTest")
+    public static Object[][] datas() {
+        return new Object[][] {
+                {"Tes1","Ha Noi","Da Nang","120000"},
+        };
+    }
 
-//    @BeforeMethod(alwaysRun = true)
-    @Before
+    @BeforeMethod(alwaysRun = true)
     public void setUp() throws MalformedURLException {
         System.out.println("start");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("appium-version", "1.7.1"); // Version Appium đang sử dụng, có hay không cũng được
         capabilities.setCapability("platformName", "Android"); //plaftFrom đang chạy (Android/IOS)
-        capabilities.setCapability("platformVersion", "6.0");// Phiên bản adnroid máy dang dung để test
+        capabilities.setCapability("platformVersion", "9");// Phiên bản adnroid máy dang dung để test
         capabilities.setCapability("deviceName", "emulator-5554"); // Tên thiết bị dùng để test
 
         capabilities.setCapability("appPackage", "vn.rure"); //App Package name
@@ -142,8 +135,7 @@ public class ExampleUnitTest {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
-//    @AfterMethod(alwaysRun = true)
-    @After
+    @AfterMethod(alwaysRun = true)
     public void afterTest() {
         System.out.println("end");
         driver.quit();
